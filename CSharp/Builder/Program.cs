e9c858a04670ebdbe3be598e99d2de17323be39f
Paul Atkins <paulatkins88@gmail.com>
2022-04-16 11:57:08 +1000

    typo

--- CSharp/Builder/Program.cs
+++ CSharp/Builder/Program.cs
@@ -4,20 +4,19 @@
 // We are going to build a couple of tacos.
 
 
-// Taco1 has everything but is also vegan
+// Taco1 has everything and is NOT vegan
 var taco1 = new Taco.Builder()
   .Meat("Beef")
   .Cheese("Tasty")
   .Shell("Hard")
-  .Vegan(false)
   .Salad(true)
   .Build();
 
 Console.WriteLine(taco1);
 
-// Taco2 has no meat and is vegan.
+// Taco2 has no meat and IS vegan.
 // The use of default values in the Taco object will give the 'Meat' property a 'None' string making the output easier to read.
-var taco2 = new Taco.Builder()  
+var taco2 = new Taco.Builder()
   .Cheese("Tasty")
   .Shell("Hard")
   .Vegan(true)
