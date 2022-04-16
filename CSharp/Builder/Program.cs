2f1e0367188bc7b966e42a9f231bd09786592f41
Paul Atkins <paulatkins88@gmail.com>
2022-04-16 11:42:31 +1000

    CSharp Builder

---
+++ CSharp/Builder/Program.cs
@@ -0,0 +1,27 @@
+﻿using Builder;
+
+// This is an example of the Builder pattern in C#.
+// We are going to build a couple of tacos.
+
+
+// Taco1 has everything but is also vegan
+var taco1 = new Taco.Builder()
+  .Meat("Beef")
+  .Cheese("Tasty")
+  .Shell("Hard")
+  .Vegan(false)
+  .Salad(true)
+  .Build();
+
+Console.WriteLine(taco1);
+
+// Taco2 has no meat and is vegan.
+// The use of default values in the Taco object will give the 'Meat' property a 'None' string making the output easier to read.
+var taco2 = new Taco.Builder()  
+  .Cheese("Tasty")
+  .Shell("Hard")
+  .Vegan(true)
+  .Salad(true)
+  .Build();
+
+Console.WriteLine(taco2);
