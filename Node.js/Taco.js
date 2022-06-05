1ca4c0690762478198caee13613a92c44bef62ac
Paul McKercher <paul.mckercher@outlook.com>
2022-06-05 12:25:20 +1000

    made similar to TS

--- Node.js/Taco.js
+++ Node.js/Taco.js
@@ -1,48 +1,37 @@
-export function taco() {
-    return "taco";
-  }
-
-class Taco {
 
 
-
+class Builder {
     constructor() {
-        this.meat = undefined;
-        this.cheese = undefined;
-        this.shell = undefined;
-        this.vegan = undefined;
-        this.salad = undefined;
+        this._taco = {
+            meat: "",
+            cheese: "",
+            shell: "",
+            vegan: false,
+            salad: false
+        };
     }
-
-
-
-
-
-
     meat(meat) {
-        this.meat = meat;
+        this._taco.meat = meat;
         return this;
     }
     cheese(cheese) {
-        this.cheese = cheese;
+        this._taco.cheese = cheese;
         return this;
     }
-
     shell(shell) {
-        this.shell = shell;
+        this._taco.shell = shell;
         return this;
     }
-
     vegan(vegan) {
-        this.vegan = vegan;
+        this._taco.vegan = vegan;
         return this;
     }
-
     salad(salad) {
-        this.salad = salad;
+        this._taco.salad = salad;
         return this;
     }
-
-
+    build() {
+        return this._taco;
+    }
 }
-
+export const TacoBuilder = TacoBuilder;
--- Node.js/index.js
+++ Node.js/index.js
@@ -1,10 +1,11 @@
-import { taco } from "./Taco.js";
-
-var tacoNew = new taco()
+import Taco from "./Taco.js";
+const BUILT_TACO = new Taco.Builder()
     .meat("chicken")
     .cheese("cheddar")
     .shell("hard")
     .vegan(false)
-    .salad(true);
+    .salad(true)
+    .build();
+console.log(BUILT_TACO);
 
-console.log(taco);
+console.log(tacoNew);
